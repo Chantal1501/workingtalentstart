7067678990ee847221f54377ecab1fe6e632e376
chantal1501 <chantalbon1501@gmail.com>
2021-02-10 12:29:32 +0100

    Toegevoegd manier waarop je zelf strings kan invoeren. Deze worden dan aan elkaar geplakt.

--- Java Opdrachten Working Talent/src/String_plakker.java
+++ Java Opdrachten Working Talent/src/String_plakker.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 public class String_plakker {
-	public final static void main(String[] args) {
-		String string1 = "Po";
-		String string2 = "ging";
-		String string3 = "en";
-		String string4 = string1 + string2 + string3;
-		System.out.println(string4);
+	public static void main(String[] args) {
+		Boolean nieuwe_string = true;
+		String concat_string = "";
+		while (nieuwe_string == true) {
+			Scanner invoer = new Scanner(System.in);
+			System.out.println("Voer een nieuwe string in");
+			String ingevoerde_string = invoer.nextLine();
+			System.out.println("Wil je nog een nieuwe string invoeren? (ja/nee)");
+			String keuze = invoer.nextLine();
+			concat_string += ingevoerde_string;
+			if (keuze.equals("nee")) {
+				nieuwe_string = false;
+			} else {
+				nieuwe_string = true;
+			}	
+		}
+		System.out.println("De samengevoegde string is: " + concat_string);
 	}
 }
