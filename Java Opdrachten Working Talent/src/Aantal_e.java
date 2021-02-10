587a8514741302135b5ac120044f16d7ff29fd8c
chantal1501 <chantalbon1501@gmail.com>
2021-02-10 14:09:11 +0100

    Opdracht aantal keer letter e toegevoegd.

--- Java Opdrachten Working Talent/src/Aantal_e.java
+++ Java Opdrachten Working Talent/src/Aantal_e.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
 
 public class Aantal_e {
-
+	public static void main(String [] args) {
+		int aantal_e = 0;
+		Scanner invoer = new Scanner(System.in);
+		System.out.println("Voer een woord in");
+		String woord = invoer.nextLine();
+		for (int x = 0; x < woord.length(); x ++) {
+			if (woord.charAt(x) == 'e') {
+				aantal_e += 1;
+			}
+		}
+		System.out.println("Er zitten " + aantal_e + " e's in het woord " + woord + ".");
+	}
 }
