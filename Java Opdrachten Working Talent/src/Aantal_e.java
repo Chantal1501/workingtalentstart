363c0a00c2e88434ff5a04fe24b1017f858ea6d1
chantal1501 <chantalbon1501@gmail.com>
2021-02-10 11:42:26 +0100

    Toegevoegd map met de java opdrachten

---
+++ Java Opdrachten Working Talent/src/Aantal_e.java
@@ -0,0 +1,4 @@
+
+public class Aantal_e {
+
+}
---
+++ Java Opdrachten Working Talent/src/String_plakker.java
@@ -0,0 +1,9 @@
+public class String_plakker {
+	public final static void main(String[] args) {
+		String string1 = "Po";
+		String string2 = "ging";
+		String string3 = "en";
+		String string4 = string1 + string2 + string3;
+		System.out.println(string4);
+	}
+}
